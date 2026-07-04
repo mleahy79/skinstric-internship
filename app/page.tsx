6ed9b5a97796636076f7864c66723f20a9384af7
Mitchell Leahy <mleahy79@icloud.com>
2026-07-04 00:18:50 -0700

    changeed: layout and global.css to include all font weight and completed nav with link

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
         <div className="font-black text-3xl  text-center mt-10">
-            Welcome to the Home Page SKINSTRIC [INDEX] i dont like you
+            
         </div>
     </main>
   )
